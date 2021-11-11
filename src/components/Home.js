bba0677520080a238877825827fe036fbb70478b
unknown <harshitbakraniya@gail.com>
2021-11-11 12:53:27 +0530

    UI Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -158,7 +158,7 @@ const Home = (props) => {
                         </Roll>
                     </div>
                 </div>
-                <video autoPlay muted loop style={{ position: "absolute", top: 0, left: 0, opacity: 0.2, height: "100vh", zIndex: 0 }}>
+                <video autoPlay muted loop style={{ position: "absolute", top: 0, left: 0, opacity: 0.2, height: "100vh", width: "100%", zIndex: 0 }}>
                     <source src={backSmoke} type="video/mp4" />
                 </video>
             </section>
